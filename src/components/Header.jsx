598713e8d1bd4e1163873bd5eee225bbcc49f603
Akshay-Tarde <akshaymtarde@gmail.com>
2023-03-11 00:39:56 +0530

    Styled the header component. Typography and layout for header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,14 @@ import React from "react";
 function Header() {
   return (
     <header className="header">
-      <img src="./troll_face.png" alt="Troll Face image" />
-      <h1 className="header-title">Meme Generator</h1>
+      <div>
+        <img
+          src="./troll_face.png"
+          alt="Troll Face image"
+          className="header-img"
+        />
+        <h1 className="header-title">Meme Generator</h1>
+      </div>
       <p className="header-text">React Course - Project 3</p>
     </header>
   );
