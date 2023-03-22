edfc0f7474d0716f0e85353152311eb7d341e9f8
Akshay-Tarde <akshaymtarde@gmail.com>
2023-03-22 23:22:45 +0530

    Added state to the page for loading images from the memes data file.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,27 +2,26 @@ import React from "react";
 import memesData from "../memesData";
 
 function Meme() {
-  function getMemeImage() {
+  const [memeImage, setMemeImage] = React.useState(""); // Setting the image url to empty
+
+  function getMemeImage(event) {
+    event.preventDefault();
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    imgUrl = memesArray[randomNumber].url;
+    setMemeImage(memesArray[randomNumber].url);
   }
 
   return (
     <main className="main">
       <form className="form" action="">
-        <input className="input" placeholder="Top Text" type="text" required />
-        <input
-          className="input"
-          placeholder="Bottom Text"
-          type="text"
-          required
-        />
+        <input className="input" placeholder="Top Text" type="text" />
+        <input className="input" placeholder="Bottom Text" type="text" />
         <br />
         <button onClick={getMemeImage} className="button">
           Get a new meme image 🖼
         </button>
       </form>
+      <img src={memeImage} alt="" />
     </main>
   );
 }
